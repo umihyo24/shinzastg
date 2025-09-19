50d8407fdaefc8798824d906453034f166304917
umihyo24 <umihyo.corporation@gmail.com>
2025-09-19 16:51:06 +0900

    Delete shiza_main directory

--- shiza_main/js/input.js
+++
@@ -1,7 +0,0 @@
-// js/input.js
-export function createKeys(){
-  const keys = {};
-  document.addEventListener("keydown", e => { keys[e.code]=true; });
-  document.addEventListener("keyup",   e => { keys[e.code]=false; });
-  return keys;
-}
--- shiza_main/js/stages/stage1.js
+++
@@ -1,9 +0,0 @@
-// js/stages/stage1.js
-export const STAGE1 = {
-  timeline: [
-    { t:0,   type:"wave", kind:"basic",  count:6, every:0.5 },
-    { t:5,   type:"wave", kind:"scout",  count:8, every:0.4 },
-    { t:12,  type:"wave", kind:"carrier",count:4, every:0.8 },
-  ],
-  length: 30
-};
